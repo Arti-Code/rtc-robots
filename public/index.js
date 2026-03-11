918c667c2bd2a65e6939a4f3e79f7d0fc88e109a
Arturo <41492101+Arti-Code@users.noreply.github.com>
2026-03-11 21:02:15 +0100

    v0.2.2

--- public/index.js
+++ public/index.js
@@ -81,7 +81,30 @@ function register_peer(name) {
 }
 function create_pc() {
     var pc = new RTCPeerConnection({
-        iceServers: [{ urls: 'stun:stun.l.google.com:19302' }]
+        "iceServers": [
+            {
+                "urls": [
+                    "stun:fr-turn8.xirsys.com",
+                ]
+            },
+            {
+                urls: [
+                    "stun:stun.l.google.com:19302"
+                ]
+            },
+            {
+                "username": "xrlEivlkdTCQvwPYbCRHDur872L9CNM7DlbAya3tEhbBcn7zMgFFN8q43pP_2v-4AAAAAGmxwT1nd296ZHlr",
+                "credential": "d05d03e4-1d7f-11f1-b1bb-be96737d4d7e",
+                "urls": [
+                    "turn:fr-turn8.xirsys.com:80?transport=udp",
+                    "turn:fr-turn8.xirsys.com:3478?transport=udp",
+                    "turn:fr-turn8.xirsys.com:80?transport=tcp",
+                    "turn:fr-turn8.xirsys.com:3478?transport=tcp",
+                    "turns:fr-turn8.xirsys.com:443?transport=tcp",
+                    "turns:fr-turn8.xirsys.com:5349?transport=tcp"
+                ]
+            }
+        ]
     });
     log("peer connection created");
     pc.oniceconnectionstatechange = function (e) {
